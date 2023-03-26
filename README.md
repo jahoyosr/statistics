666af629dfff9e47904d459152a6fa07fbb80530
Julian Hoyos <jahoyosr@gmail.com>
2023-03-26 18:12:43 +0200

    Adding code implementation for C1M1 Assessment

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-/* Add Author and Project Details here */
+# Statistics Functions for Unsigned Char Arrays
+
+This project implements several statistics functions for unsigned char arrays, including the median, mean, maximum, and minimum. Additionally, there is a utility function provided to sort the array in descending order.
+
+## Usage
+To use the statistics functions in your own code, include the stats.h header file and link against the stats.c source file. Then, call the functions as needed with your own unsigned char arrays.
+
+```
+#include "stats.h"
+
+unsigned char my_array[SIZE] = { ... };
+
+print_statistics(my_array, SIZE);
+sort_array(my_array, SIZE);
+print_array(my_array, SIZE);
+```
+
+##  Function Descriptions
+`print_statistics(unsigned char* array_pointer, unsigned int array_size)`
+Prints the median, mean, maximum, and minimum values of the provided unsigned char array to the console.
+
+`print_array(unsigned char* array_pointer, unsigned int array_size)`
+Prints each element of the provided unsigned char array to the console, with the element's index and value.
+
+`find_median(unsigned char* array_pointer, unsigned int array_size)`
+Finds the median value of the provided unsigned char array.
+
+`find_mean(unsigned char* array_pointer, unsigned int array_size)`
+Finds the mean value of the provided unsigned char array.
+
+`find_maximum(unsigned char* array_pointer, unsigned int array_size)`
+Finds the maximum value of the provided unsigned char array.
+
+`find_minimum(unsigned char* array_pointer, unsigned int array_size)`
+Finds the minimum value of the provided unsigned char array.
+
+`sort_array(unsigned char* array_pointer, unsigned int array_size)`
+Sorts the provided unsigned char array in descending order.
+
+# License
+This code is released under the terms of the MIT License.
+
+# Contact
+If you'd like to contact me, please send me an email.
+jahoyosr@gmail.com
--- stats.c
+++ stats.c
@@ -9,56 +9,145 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c 
+ * @brief File containing the defintion of statistics functions for an unsigned char array. 
  *
- * <Add Extended Description Here>
+ * Contains the implementation of several statistics functions for unsigned char arrays, 
+ * including the median, mean, maximum, and minimum. Additionally, there is a utility function 
+ * provided to sort the array in descending order.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Julian Hoyos
+ * @date 27/03/2023
  *
  */
 
-
-
 #include <stdio.h>
+#include <string.h>
 #include "stats.h"
 
 /* Size of the Data Set */
 #define SIZE (40)
+
+/* Function definition*/
 void print_statistics(unsigned char* array_pointer, unsigned int array_size)
 {
-  /* To do */
+  unsigned char median;
+  unsigned char mean;
+  unsigned char maximum;
+  unsigned char minimum;
+
+  median = find_median(array_pointer, array_size);
+  mean = find_mean(array_pointer, array_size);
+  maximum = find_maximum(array_pointer, array_size);
+  minimum = find_minimum(array_pointer, array_size);
+
+  printf("Median: %d  - Mean: %d  - Maximum: %d - Minimum: %d \n", median, mean, maximum, minimum);
+
 }
 
 void print_array(unsigned char* array_pointer, unsigned int array_size)
 {
-  /* To do */
+  unsigned char i;
+  for(i = 0; i < array_size ; ++i)
+  {
+    printf("%dth element: %d\n", i, *(array_pointer+i));
+  }
 }
 
 unsigned char find_median(unsigned char* array_pointer, unsigned int array_size)
 {
-  /* To do */
+  unsigned char median;
+  unsigned char mid_element;
+  unsigned char sorted_array[array_size];
+  unsigned char* sorted_array_pointer = sorted_array;
+
+  /* Copy array */
+  memcpy(sorted_array_pointer, array_pointer, array_size * sizeof(unsigned char));
+  mid_element = array_size/2;
+ 
+  if(0 != array_size % 2 )
+  {
+    median = *(sorted_array_pointer+mid_element);
+  }
+  else
+  {
+    median = (*(sorted_array_pointer+mid_element) + *(sorted_array_pointer+mid_element-1))/2;
+  }
+
+  return median;
 }
 
 unsigned char find_mean(unsigned char* array_pointer, unsigned int array_size)
 {
-  /* To do */
+  unsigned int sum_value = 0;
+  unsigned char mean = 0;
+  unsigned char i;
+
+  for(i = 0; i < array_size ; ++i)
+  {
+    sum_value += *(array_pointer+i);
+  }
+
+  mean = (unsigned char)(sum_value/array_size);
+
+  return mean;
 }
 
 unsigned char find_maximum(unsigned char* array_pointer, unsigned int array_size)
 {
-  /* To do */
+  unsigned char maximum = 0;
+  unsigned char i;
+
+  for(i = 0; i < array_size ; ++i)
+  {
+    maximum = maximum < *(array_pointer+i) ? *(array_pointer+i) : maximum;
+  }
+
+  return maximum;
 }
 
 unsigned char find_minimum(unsigned char* array_pointer, unsigned int array_size)
 {
-  /* To do */
+  unsigned char minimum = 255;
+  unsigned char i;
+
+  for(i = 0; i < array_size ; ++i)
+  {
+    minimum = minimum > *(array_pointer+i) ? *(array_pointer+i) : minimum;
+  }
+
+  return minimum;
 }
 
 void sort_array(unsigned char* array_pointer, unsigned int array_size)
 {
-  /* To do */
+  unsigned char i;
+  unsigned char j;
+  unsigned char current_value;
+  unsigned char max_value;
+  unsigned char max_index;
+  
+
+  for(i = 0; i < array_size ; ++i)
+  {
+    current_value = *(array_pointer+i);
+    max_value = current_value;
+
+    for(j = i; j < array_size ; ++j)
+    {
+      if( max_value < *(array_pointer+j))
+      {
+        max_value = *(array_pointer+j);
+        max_index = j;
+      }
+    }
+
+    if(max_value != current_value)
+    {
+      *(array_pointer+i) = max_value;
+      *(array_pointer+max_index) = current_value;
+    }
+  }
 
 }
 
@@ -70,9 +159,10 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+
+  print_statistics(test, SIZE);
+  sort_array(test, SIZE);
+  print_array(test,SIZE);
 
 }
 
-/* Add other Implementation File Code Here */
